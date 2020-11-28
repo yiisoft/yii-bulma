1b9faab4dbd039e584fd5043d2986b81af0e5806
Vitalii Shkolin <vitalii@shkolin.net.ua>
2020-11-28 09:01:04 +0200

    Update tests.

--- tests/ModalCardTest.php
+++ tests/ModalCardTest.php
@@ -12,14 +12,12 @@ final class ModalCardTest extends TestCase
     {
         ModalCard::counter(0);
 
-        $html = ModalCard::begin()
-            ->toggleButtonEnabled(false)
-            ->start();
+        $html = ModalCard::begin()->start();
         $html .= 'Say hello...';
         $html .= ModalCard::end();
 
         $expectedHtml = <<<HTML
-
+<button type="button" class="button" data-target="#w1-modal" aria-haspopup="true">Toggle button</button>
 <div id="w1-modal" class="modal">
 <div class="modal-background"></div>
 <div class="modal-card">
@@ -30,6 +28,34 @@ final class ModalCardTest extends TestCase
 <section class="modal-card-body">
 Say hello...</section>
 <footer class="modal-card-foot"></footer>
+</div>
+</div>
+HTML;
+
+        $this->assertEqualsWithoutLE($expectedHtml, $html);
+    }
+
+    public function testFooterOptions(): void
+    {
+        ModalCard::counter(0);
+
+        $html = ModalCard::begin()
+            ->footerOptions(['class' => 'bg-transparent'])
+            ->start();
+        $html .= ModalCard::end();
+
+        $expectedHtml = <<<HTML
+<button type="button" class="button" data-target="#w1-modal" aria-haspopup="true">Toggle button</button>
+<div id="w1-modal" class="modal">
+<div class="modal-background"></div>
+<div class="modal-card">
+<header class="modal-card-head">
+<p class="modal-card-title"></p>
+<button type="button" class="delete" aria-label="close"></button>
+</header>
+<section class="modal-card-body">
+</section>
+<footer class="modal-card-foot bg-transparent"></footer>
 </div>
 </div>
 HTML;
