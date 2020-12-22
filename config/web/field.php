6556c53bbbce142d79f9646360a8e5b191779ba1
Mr.42 <johan@mr42.me>
2020-12-22 20:08:02 +0100

    Form fields styling

---
+++ config/web/field.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Composer\InstalledVersions;
+use Yiisoft\Form\Widget\Field;
+
+if (InstalledVersions::isInstalled('yiisoft/forms') && $params['yiisoft/form']['bulma']['enabled'] === true) {
+    return [
+        Field::class => fn () => Field::Widget($params['yiisoft/form']['bulma']['fieldConfig']),
+    ];
+}
