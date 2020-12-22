0e7cfe18d73bbb658f8ffcab5370d635e9b6f295
Mr.42 <johan@mr42.me>
2020-12-22 20:39:06 +0100

    Correct package name

--- config/web/field.php
+++ config/web/field.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Composer\InstalledVersions;
 use Yiisoft\Form\Widget\Field;
 
-if (InstalledVersions::isInstalled('yiisoft/forms') && $params['yiisoft/form']['bulma']['enabled'] === true) {
+if (InstalledVersions::isInstalled('yiisoft/form') && $params['yiisoft/form']['bulma']['enabled'] === true) {
     return [
         Field::class => fn () => Field::Widget($params['yiisoft/form']['bulma']['fieldConfig']),
     ];
