134de1083e1368f427a1019e1c651075d259fe03
Alexander Makarov <sam@rmcreative.ru>
2021-04-13 01:47:24 +0300

    Adjust namespaces

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,7 +16,7 @@ use Yiisoft\Assets\AssetManager;
 use Yiisoft\Assets\AssetPublisher;
 use Yiisoft\Assets\AssetPublisherInterface;
 use Yiisoft\Di\Container;
-use Yiisoft\Factory\Definitions\Reference;
+use Yiisoft\Factory\Definition\Reference;
 use Yiisoft\Files\FileHelper;
 use Yiisoft\Widget\WidgetFactory;
 
