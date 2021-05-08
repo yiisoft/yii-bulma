7912283d3d6a89ff52b9c5c873bd264d7c51766a
Alexander Makarov <sam@rmcreative.ru>
2021-05-08 22:16:18 +0300

    Make closure static, fix method call case

--- config/web/field.php
+++ config/web/field.php
@@ -6,6 +6,6 @@ use Yiisoft\Form\Widget\Field;
 
 if ($params['yiisoft/form']['bulma']['enabled'] === true) {
     return [
-        Field::class => fn () => Field::Widget($params['yiisoft/form']['bulma']['fieldConfig']),
+        Field::class => static fn () => Field::widget($params['yiisoft/form']['bulma']['fieldConfig']),
     ];
 }
