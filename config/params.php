613d69ff1ef25342fb646d4ac497dbe1d26b3165
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2021-12-24 17:43:03 -0300

    Fix config (#55)

--- config/params.php
+++ config/params.php
@@ -2,23 +2,20 @@
 
 declare(strict_types=1);
 
-/**
- * @link https://bulma.io/documentation/form/general/#complete-form-example
- */
+/** @link https://bulma.io/documentation/form/general/#complete-form-example */
+
 return [
     'yiisoft/form' => [
         'bulma' => [
             'enabled' => true,
-            'fieldConfig' => [
-                'containerClass' => 'field',
-                'errorClass' => 'has-text-danger is-italic',
-                'hintClass' => 'help',
-                'inputClass' => 'input',
-                'invalidClass' => 'has-background-danger',
-                'labelClass' => 'label',
-                'template' => "{label}<div class=\"control\">\n{input}</div>\n{hint}\n{error}",
-                'validClass' => 'has-background-success',
-            ],
+            'containerClass' => 'field',
+            'errorClass' => 'has-text-danger is-italic',
+            'hintClass' => 'help',
+            'inputClass' => 'input',
+            'invalidClass' => 'has-background-danger',
+            'labelClass' => 'label',
+            'template' => "{label}<div class=\"control\">\n{input}</div>\n{hint}\n{error}",
+            'validClass' => 'has-background-success',
         ],
     ],
 ];
